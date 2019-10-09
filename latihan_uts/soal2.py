cb74e9b78e676b02e4764217ca64403d85bbbf97
sonadztux <nanda@anakdesa.id>
2019-10-09 17:32:19 +0700

    Menambahkan komentar untuk output dan pembahasan

--- latihan_uts/soal2.py
+++ latihan_uts/soal2.py
@@ -4,3 +4,15 @@ c = a*b
 print(c)
 
 # Guess the output and explain the algorithm !
+
+""" 
+OUTPUT:
+sekut sekut sekut sekut sekut
+
+Pembahasan:
+Dalam bahasa pemrograman python data bertipe String dapat dilakukan operasi
+perkalian dengan data bertipe integer. 
+
+Sehingga variabel a yang berisi String 'sekut' dapat dikalikan sebanyak 5x
+menjadi 'sekut sekut sekut sekut sekut'
+"""
